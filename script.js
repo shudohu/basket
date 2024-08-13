909368e0fd3a621219bbe51534e80f2322ceae32
shudohu <kaigarakudai@gmail.com>
2024-08-13 14:53:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    if (password === "yourpassword") {
+        window.location.href = "protected.html";
+    } else {
+        alert("パスワードが間違っています。");
+    }
+}
