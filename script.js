f8c39dde1f56241b3afb3a92746ba952758d2626
shudohu <kaigarakudai@gmail.com>
2024-08-13 15:13:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function promptPassword() {
     var password = prompt("パスワードを入力してください:");
-    if (password === "yourpassword") {
+    if (password === "your") {
         window.location.href = "https://shudohu.github.io/basket/team1";
     } else {
         alert("パスワードが間違っています。");
